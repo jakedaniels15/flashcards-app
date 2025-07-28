482654efe54be4caa8d4b01c63dd7c11ea754b51
jakedaniels15 <jake.daniels15@hotmail.co.uk>
2025-07-28 10:32:58 +0100

    Add prepopulated stock topics
    - Added 6 stock topics: Science, Nature, Geography, Sports, Music, History
    - Each topic has appropriate icon from existing icon set:
      * Science: Medicine icon
      * Nature: Leaves icon
      * Geography: Sun icon
      * Sports: Shuttlecock icon
      * Music: Crayons icon
      * History: Calendar icon
    - Topics now appear on Topics page by default
    - Users can still create additional custom topics

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,6 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { 
+  MEDICINE_ICON,
+  LEAVES_ICON,
+  SUN_ICON,
+  SHUTTLECOCK_ICON,
+  CRAYONS_ICON,
+  CALENDAR_ICON
+} from "../../data/icons";
 
-const initialState = {};
+// Stock topics with predefined IDs and appropriate icons
+const stockTopics = {
+  "science-topic": {
+    id: "science-topic",
+    name: "Science",
+    icon: MEDICINE_ICON,
+    quizIds: []
+  },
+  "nature-topic": {
+    id: "nature-topic", 
+    name: "Nature",
+    icon: LEAVES_ICON,
+    quizIds: []
+  },
+  "geography-topic": {
+    id: "geography-topic",
+    name: "Geography", 
+    icon: SUN_ICON,
+    quizIds: []
+  },
+  "sports-topic": {
+    id: "sports-topic",
+    name: "Sports",
+    icon: SHUTTLECOCK_ICON,
+    quizIds: []
+  },
+  "music-topic": {
+    id: "music-topic",
+    name: "Music",
+    icon: CRAYONS_ICON,
+    quizIds: []
+  },
+  "history-topic": {
+    id: "history-topic",
+    name: "History",
+    icon: CALENDAR_ICON,
+    quizIds: []
+  }
+};
+
+const initialState = stockTopics;
 
 export const topicsSlice = createSlice({
   name: "topics",
